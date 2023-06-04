f028b40bff385753a87c1c1549f988ec0ab904a1
Laxmikant <guravlaxmikant01@gmail.com>
2023-06-04 19:54:52 +0530

    11

---
+++ 11_longest_consecutive_sequence.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+int lengthOfLongestConsecutiveSequence(vector<int> &a, int n)
+{
+    int longest = 1;
+    if (n == 0)
+        return 0;
+    unordered_set<int> st;
+    for (int i = 0; i < n; i++)
+    {
+        st.insert(a[i]);
+    }
+    for (auto i : st)
+    {
+        if (st.find(i - 1) == st.end())
+        {
+            int cnt = 1;
+            int x = i;
+            while (st.find(x + 1) != st.end())
+            {
+                cnt++;
+                x++;
+            }
+            longest = max(longest, cnt);
+        }
+    }
+    return longest;
+}
