3e582423a69d0687a353ebbf7ed1928f51ad26c5
Laxmikant <guravlaxmikant01@gmail.com>
2023-06-05 10:08:37 +0530

    16th

---
+++ 16_middle_of_linked_list.cpp
@@ -0,0 +1,15 @@
+Node *findMiddle(Node *head)
+{
+    Node *slow = head;
+    Node *fast = head;
+    while (fast != NULL)
+    {
+        fast = fast->next;
+        if (fast != NULL)
+        {
+            fast = fast->next;
+            slow = slow->next;
+        }
+    }
+    return slow;
+}
