69fc45d7e7d97c01dcbc46e386194f636c77fa31
Laxmikant <guravlaxmikant01@gmail.com>
2023-06-06 21:03:07 +0530

    40th

---
+++ 40_rotate_linked_list.cpp
@@ -0,0 +1,24 @@
+Node *rotate(Node *head, int k)
+{
+    if (head == NULL || head->next == NULL || k == 0)
+        return head;
+    // calculating length
+    Node *temp = head;
+    int length = 1;
+    while (temp->next != NULL)
+    {
+        ++length;
+        temp = temp->next;
+    }
+    // link last node to first node
+    temp->next = head;
+    k = k % length;       // when k is more than length of list
+    int end = length - k; // to get end of the list
+    while (end--)
+        temp = temp->next;
+    // breaking last node link and pointing to NULL
+    head = temp->next;
+    temp->next = NULL;
+
+    return head;
+}
